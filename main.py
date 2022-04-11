566b15306388e64fe308c36b238d9db9e767dfd7
axrasp <aaraspopin@gmail.com>
2022-04-11 15:11:02 +0300

    add custom path argument

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import argparse
 import collections
 import datetime
+import os
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 import pandas
+from dotenv import load_dotenv
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+load_dotenv()
+catalog_path = os.getenv('CATALOG_PATH')
+
+parser = argparse.ArgumentParser(
+    description='Сайт магазина авторского вина "Новое русское вино"'
+)
+parser.add_argument('-c', '--catalog', default='catalog/wine3.xlsx', help='Укажите путь к каталогу (вместе с названием файла)')
+args = parser.parse_args()
+if args.catalog:
+    catalog_path = args.catalog
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -11,7 +25,7 @@ env = Environment(
 )
 
 template = env.get_template('template.html')
-products = pandas.read_excel('wine3.xlsx', sheet_name='Лист1', usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'], na_values='None', keep_default_na=False).to_dict(orient='record')
+products = pandas.read_excel(catalog_path, sheet_name='Лист1', usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'], na_values='None', keep_default_na=False).to_dict(orient='record')
 products_grouped = collections.defaultdict(list)
 for product in products:
     products_grouped[product['Категория']].append(product)
